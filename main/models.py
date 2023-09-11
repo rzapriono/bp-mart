13dcd208c340f4a0ef6ce883a3e4f9bc233b4056
rzapriono <reza.apriono@ui.ac.id>
2023-09-11 22:56:12 +0700

    created app main, MVT

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    description = models.TextField()
+    price = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    purchased_from = models.CharField(max_length=255)
