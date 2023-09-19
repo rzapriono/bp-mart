0a1aaefea68d971735268227919d536466cc9327
rzapriono <reza.apriono@ui.ac.id>
2023-09-20 00:07:04 +0700

    add BASE_DIR / 'templates' to TEMPLATES

--- bp_mart/settings.py
+++ bp_mart/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bp_mart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
